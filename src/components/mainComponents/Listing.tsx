82e3de5203ba116da961d36b3e9c0ba12fc3333c
Medet Ibrashov <ibrashov1305@gmail.com>
2022-12-24 16:55:11 +0600

    add scrollbar for participation list

--- src/components/mainComponents/Listing.tsx
+++ src/components/mainComponents/Listing.tsx
@@ -17,40 +17,42 @@ const Listing: FC<ListingProps> = ({
   const navigate = useNavigate();
 
   return (
-    <div className="listing">
+    <div className="listing-wrapper">
       <h3>Participation listing (enable only for participants)</h3>
-
-      <table>
-        <tr>
-          <th>
-            <MySubtitle>Name</MySubtitle>
-          </th>
-          <th>
-            <MySubtitle>Email</MySubtitle>
-          </th>
-          <th>
-            <MySubtitle>Wallet</MySubtitle>
-          </th>
-        </tr>
-        {users.map((userItem) => (
-          <tr
-            onClick={() =>
-              userItem.id !== createdUser.id && navigate(`/user/${userItem.id}`)
-            }
-            className={userItem.id !== createdUser.id ? "user-item" : ""}
-            key={userItem.id}
-          >
-            <td>{userItem.username}</td>
-            <td>{userItem.email}</td>
-            <td>{userItem.address}</td>
-            {createdUser.id !== 0 && userItem.id === createdUser.id && (
-              <td onClick={() => removeNewUser(userItem.id)}>
-                <i className="fa-solid fa-xmark"></i>
-              </td>
-            )}
+      <div className="listing">
+        <table>
+          <tr>
+            <th>
+              <MySubtitle>Name</MySubtitle>
+            </th>
+            <th>
+              <MySubtitle>Email</MySubtitle>
+            </th>
+            <th>
+              <MySubtitle>Wallet</MySubtitle>
+            </th>
           </tr>
-        ))}
-      </table>
+          {users.map((userItem) => (
+            <tr
+              onClick={() =>
+                userItem.id !== createdUser.id &&
+                navigate(`/user/${userItem.id}`)
+              }
+              className={userItem.id !== createdUser.id ? "user-item" : ""}
+              key={userItem.id}
+            >
+              <td>{userItem.username}</td>
+              <td>{userItem.email}</td>
+              <td>{userItem.address}</td>
+              {createdUser.id !== 0 && userItem.id === createdUser.id && (
+                <td onClick={() => removeNewUser(userItem.id)}>
+                  <i className="fa-solid fa-xmark"></i>
+                </td>
+              )}
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 };
